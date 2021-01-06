8f246e2f20fb2d4adf6901eccc719d1803769d6b
OmarSalah95 <Omars.e.995@gmail.com>
2021-01-06 10:13:50 -0800

    Initial commit initializing Repo COC.cpp completed and copied for templating in myinitials.cpp

---
+++ project-1/COC.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << " CC   OO    CC\nC    O  O  C\nC    O  O  C\n CC   OO    CC\n \n";
+
+
+    // cout << " CC\nC\nC\n CC\n";
+    // cout << " OO\nO  O\nO  O\n OO\n"; 
+    // cout << " CC\nC\nC\n CC\n";
+
+    return 0;
+}
