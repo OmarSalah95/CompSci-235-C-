3f916c4384f43bc2f387774fd6f17545289be42a
OmarSalah95 <Omars.e.995@gmail.com>
2021-01-06 11:15:16 -0800

    Method 2 implemented for MyInitials.cpp

--- project-1/MyInitials.cpp
+++ project-1/MyInitials.cpp
@@ -15,21 +15,34 @@ using namespace std;
 
 int main()
 {
-    // const string topAndBottom = string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E');
+    const string 
+        topAndBottom = string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E'),
+        pattern1 = "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' '),
+        middle = "O" + string(6,' ') + "O    " + string(8,'S') + "    " + string(8,'E'),
+        pattern2 = "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ');
 
 
-    // for(int i=0; i<8; i++)
-    // {
+    for(int i=0; i<8; i++)
+    {
+        i==0 or i == 7 
+            ? cout << topAndBottom + "\n" 
+        : i>0 and i<4 
+            ? cout << pattern1 + "\n" 
+        :i == 4
+            ? cout << middle + "\n" 
+        : cout << pattern2 + "\n" ;
+    }
 
-    // }
-    cout << string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E') << endl;
-    cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
-    cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
-    cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
-    cout << "O" + string(6,' ') + "O    " + string(8,'S') + "    " + string(8,'E') << endl;
-    cout << "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ') << endl;
-    cout << "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ') << endl;
-    cout << string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E') << endl;
+    // This is one very repetative method of doing this, it will work if uncommented.
+    // cout << string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E') << endl;
+    // cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
+    // cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
+    // cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
+    // cout << "O" + string(6,' ') + "O    " + string(8,'S') + "    " + string(8,'E') << endl;
+    // cout << "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ') << endl;
+    // cout << "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ') << endl;
+    // cout << string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E') << endl;
+    
     return 0;
     
 }
