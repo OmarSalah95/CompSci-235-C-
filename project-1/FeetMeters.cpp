6fd78f2bd6dbec07ba7bdea0f99542fe9875f72e
OmarSalah95 <Omars.e.995@gmail.com>
2021-01-06 12:27:31 -0800

    Feet Meters done

---
+++ project-1/FeetMeters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float f;
+    cout << "Please enter the number of feet you would like to convert to Meters: \n";
+    cin >> f;
+    cout << f << " Feet is equal to " << f/3.05 <<" meters.";
+    return 0;
+}
