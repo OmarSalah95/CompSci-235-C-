405fa021f3dca6e1aa3649ebb854b101184bba4e
OmarSalah95 <Omars.e.995@gmail.com>
2021-01-06 10:57:31 -0800

    Added the 8 cout method of printing out my initials, I am actually going to refactor this to be more dry using a 4 loop and the repeated patterns in displayingthe intials to console.

--- project-1/MyInitials.cpp
+++ project-1/MyInitials.cpp
@@ -15,7 +15,21 @@ using namespace std;
 
 int main()
 {
-    cout << string(8,'O') + "    " + string(8,'S');
+    // const string topAndBottom = string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E');
 
+
+    // for(int i=0; i<8; i++)
+    // {
+
+    // }
+    cout << string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E') << endl;
+    cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
+    cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
+    cout << "O" + string(6,' ') + "O    S" + string(11,' ') + "E    " + string(7,' ') << endl;
+    cout << "O" + string(6,' ') + "O    " + string(8,'S') + "    " + string(8,'E') << endl;
+    cout << "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ') << endl;
+    cout << "O" + string(6,' ') + "O    " + string(7,' ') + "S    E" + string(7,' ') << endl;
+    cout << string(8,'O') + "    " + string(8,'S') + "    " + string(8,'E') << endl;
     return 0;
+    
 }
